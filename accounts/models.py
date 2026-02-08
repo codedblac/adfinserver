eb6b608ea8ffa9b661c6286d23efbbf9b8e8c5cb
CODED BLACK <codedb693@gmail.com>
2026-02-08 20:15:03 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -65,7 +65,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 
 # Address Model
-# ---------------------------
+
 class Address(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="addresses")
     full_name = models.CharField(max_length=255)
