88e068fdee4e72e009b73e819ffdc118a7734511
CODED BLACK <codedb693@gmail.com>
2026-02-12 23:14:22 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -35,7 +35,7 @@ class UserManager(BaseUserManager):
 
 
 
-# Custom User Model
+#  User Model
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
