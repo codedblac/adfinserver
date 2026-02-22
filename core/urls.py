38efad09f4f269916461d7065977c5bfa20a9a67
CODED BLACK <codedb693@gmail.com>
2026-02-22 16:52:23 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path("api/health/", health_check, name="health-check"),
 ]
 
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
