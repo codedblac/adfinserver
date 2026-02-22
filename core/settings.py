9d8788e202261363a41c94e5c4b04b44683181ae
CODED BLACK <codedb693@gmail.com>
2026-02-22 16:49:20 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
