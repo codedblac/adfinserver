87ee413db5cd34d2a1fb4838c569e7365fac430d
CODED BLACK <codedb693@gmail.com>
2026-02-22 16:49:03 +0300

    Update asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -21,6 +21,8 @@ from django.core.asgi import get_asgi_application
 # Set the default Django settings module for the ASGI application
 
 
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 
