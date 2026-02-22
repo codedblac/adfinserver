660f8ac150dfd83910479b24e307c7c2e055942e
CODED BLACK <codedb693@gmail.com>
2026-02-22 16:51:33 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
 ]
 
 
-MIDDLEWARE = [
+
+
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
